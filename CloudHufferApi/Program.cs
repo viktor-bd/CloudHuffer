2a2cb2926db4084acbcf1822366b45dbb9fe8541
viktor-bd <viktordorph@hotmail.com>
2025-06-26 03:20:39 +0200

    Refactor Swagger configuration and remove deprecated OpenAPI methods

--- CloudHufferApi/Program.cs
+++ CloudHufferApi/Program.cs
@@ -3,29 +3,28 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddScoped<CloudHufferApi.Services.IProbeTextParserService, CloudHufferApi.Services.ProbeTextParserService>();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "CloudHuffer API", Version = "v1" });
-}); // Add this for Swagger UI
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.UseSwagger(); // Add this for Swagger UI
+    app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "CloudHuffer API v1");
-    }); // Add this for Swagger UI
+    });
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 var summaries = new[]
 {
